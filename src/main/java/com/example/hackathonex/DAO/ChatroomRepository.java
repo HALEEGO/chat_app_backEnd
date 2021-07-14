02779f4c3597a8c950811962df18ce46099ef22f
2sssg <lsg020300@naver.com>
2021-07-14 15:30:56 +0900

    Delete : delete temp file
    ChatroomRepository -> Delete

--- src/main/java/com/example/hackathonex/DAO/ChatroomRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.hackathonex.DAO;
-
-public class ChatroomRepository {
-
-
-}
--- src/main/java/com/example/hackathonex/constant/Url.java
+++
@@ -1,6 +0,0 @@
-package com.example.hackathonex.constant;
-public class Url {
-    public String Endpoint = "/chat";
-    public String topic = "/topic";
-    public String App = "/app";
-}
