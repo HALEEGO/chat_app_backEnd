d07051155f8de1571852c56af9bb4b8d3097e771
2sssg <lsg020300@naver.com>
2021-07-16 17:20:10 +0900

    Refactor : chatController
    code refactoring

--- src/main/java/com/example/hackathonex/controller/ChatController.java
+++ src/main/java/com/example/hackathonex/controller/ChatController.java
@@ -2,8 +2,11 @@ package com.example.hackathonex.controller;
 
 import com.example.hackathonex.DTO.ChatMessageDTO;
 import com.example.hackathonex.constant.ConstantURL;
+import com.example.hackathonex.constant.Enum.MessageType;
 import com.example.hackathonex.constant.Returns;
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.bcel.Const;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
@@ -13,22 +16,24 @@ import org.springframework.stereotype.Controller;
 @RequiredArgsConstructor
 @Controller
 public class ChatController {
-
-    Returns returns;
+    Returns returns = new Returns();
+    @Autowired
     private final SimpMessageSendingOperations messagingTemplate;
 
 
-
-    @MessageMapping("/send_chat") // 이름, context  -> toJSON
+    @MessageMapping("/sendchat") // 이름, context  -> toJSON
     public void send_chat(ChatMessageDTO message) {
-        returns = new Returns(ConstantURL.topic);
 
-        returns.append("/send_chat/").append(message.getRoomID());
-        returns.put("userName", message.getUserName())
+        returns
+                .append(ConstantURL.topic)
+                .append("/sendchat/")
+                .append(message.getRoomID());
+        returns
+                .put("userName", message.getUser().getUserName())
                 .put("context", message.getContext());
 
-        messagingTemplate.convertAndSend(returns.getReturnURL().toString(), returns.getReturnValue());
+        if (message.getMessagetype() == MessageType.TALK) {
+            messagingTemplate.convertAndSend(returns.getReturnURL().toString(), returns.getReturnValue());
+        }
     }
-
-
 }
