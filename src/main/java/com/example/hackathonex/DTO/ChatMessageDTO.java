32f414717cdbf407703195d81132968deed175a5
2sssg <lsg020300@naver.com>
2021-07-20 17:10:32 +0900

    Chore: DTOChanged
    String roomID -> RoomDTO room

--- src/main/java/com/example/hackathonex/DTO/ChatMessageDTO.java
+++ src/main/java/com/example/hackathonex/DTO/ChatMessageDTO.java
@@ -3,16 +3,18 @@ package com.example.hackathonex.DTO;
 import com.example.hackathonex.constant.Enum.MessageType;
 import com.example.hackathonex.constant.Enum.MoveType;
 import com.example.hackathonex.model.User;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
-@Getter
-@Setter
+@Data
+@NoArgsConstructor
 public class ChatMessageDTO {
     private MessageType messagetype; // 메시지 타입
     private MoveType movetype; // 이동 타입
-    private  String roomID; // 방번호
+    private  RoomDTO room; // 방번호
     private UserDTO user ; // 메시지 보낸사람
     private String context; // 메시지
 
--- src/main/java/com/example/hackathonex/DTO/RoomDTO.java
+++ src/main/java/com/example/hackathonex/DTO/RoomDTO.java
@@ -1,10 +1,12 @@
 package com.example.hackathonex.DTO;
 
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
+@Data
+@NoArgsConstructor
 public class RoomDTO {
     private String roomID;
 }
