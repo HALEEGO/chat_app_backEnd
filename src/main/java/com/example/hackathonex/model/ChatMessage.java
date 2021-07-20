e0c798c12b768b8d42cef942c31324d361397156
2sssg <lsg020300@naver.com>
2021-07-20 17:11:28 +0900

    Chore: Changed model
    String roomID -> Room room

--- src/main/java/com/example/hackathonex/model/ChatMessage.java
+++ src/main/java/com/example/hackathonex/model/ChatMessage.java
@@ -9,7 +9,7 @@ public class ChatMessage {
 
     private MessageType messagetype; // 메시지 타입
     private MoveType movetype; // 이동 타입
-    private  String roomID; // 방번호
+    private  Room room; // 방번호
     private User userName ; // 메시지 보낸사람
     private String context; // 메시지
 
